8bc441f033d1c8d6d5b5a0697f3601fc002a00ba
lily <lily@transgirls.love>
2026-06-15 16:43:47 -0700

    upd: lazily commit on June 15th, 2026 4:43 PM UTC -0700 by lily

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,16 +1,14 @@
 export default function Footer() {
   return (
     <>
-      <div className="mt-10 w-full border-t border-white/20" />
+      <div className="mt-4 w-full border-t border-white/10" />
       <div className="px-10 py-6 w-screen flex flex-row justify-between items-center max-md:justify-center max-md:flex-col">
         <div className="px-2 py-2 flex flex-row items-center gap-2">
           <div>
             <p className="text-xs text-left font-bold">
               <span className="text-[#f8bfd4]">lumin</span>.rest
             </p>
-            <p className="text-muted-foreground text-xs">
-              Originally known as lumin.rocks
-            </p>
+
           </div>
         </div>
         <div className="flex flex-col items-end gap-1 max-md:items-center max-md:mt-5">
