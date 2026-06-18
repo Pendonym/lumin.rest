e849f21dc29ecefdd2ef9c4cd88163bd0fb45061
lily ❤︎⁠ <lily@transgirls.love>
2026-06-18 13:56:16 -0700

    Update loadstrings.ts

--- src/data/loadstrings.ts
+++ src/data/loadstrings.ts
@@ -1,3 +1,3 @@
 export const loadstringList = {
-  main: 'loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/17c67154fb369705052cd2dd63dc1bb7.lua"))())',
+  main: 'loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/17c67154fb369705052cd2dd63dc1bb7.lua"))()',
 };
